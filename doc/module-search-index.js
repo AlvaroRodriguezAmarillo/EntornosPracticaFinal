8c6d279aa7a0aca5948ebca714fd03972a4380f3
Álvaro Rodríguez Amarillo <145559192+AlvaroRodriguezAmarillo@users.noreply.github.com>
2024-05-28 13:47:57 +0200

    Javadoc realizado

---
+++ doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"Digimones"}];updateSearchResults();
---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"m":"Digimones","l":"actividad"},{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();
