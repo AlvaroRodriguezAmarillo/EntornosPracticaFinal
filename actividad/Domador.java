aa5165a32147f9a10c18629acc8a5afb19c41b8b
Álvaro Rodríguez Amarillo <145559192+AlvaroRodriguezAmarillo@users.noreply.github.com>
2024-05-28 12:34:33 +0200

    Add files via upload
    Domador comentado

--- actividad/Domador.java
+++ actividad/Domador.java
@@ -3,17 +3,39 @@ package actividad;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * En la clase domador, el domador tendrá un equipo de Digimones y
+ * puede capturar nuevos Digimones
+ * 
+ * @version 1.0
+ * @author Alvaro
+ */
+
 public class Domador {
 	String Nombre;
 	ArrayList<DigimonesIconicos>Equipo;
 	Random Aleatorio = new Random();
 
+	 /**
+     * En el constructor de la clase Domador se declara el nombre del domador y se 
+     * añade un digimon aleatorio al equipo del domador
+     * 
+     * @param nombre Nombre del domador
+     */
+	
 	public Domador(String nombre) {
 		this.Nombre = nombre;
 		this.Equipo = new ArrayList<>();
 		this.Equipo.add(DigimonAleatorio());
 	}
 
+	
+	 /**
+     * El método DigimonAleatorio genera un digimon aleatorio y lo añade al equipo.
+     * 
+     * @return Devuelve un objeto 
+     */
+	
 	public DigimonesIconicos DigimonAleatorio() {
 		String[] nombreDigimon = {"Agumon", "Gabumon", "Patamon"};
 		String[] especieDigimon = {"Reptil", "Reptil", "Mammal"};
@@ -21,6 +43,13 @@ public class Domador {
 		return new DigimonesIconicos(nombreDigimon[index], especieDigimon[index]);
 	}
 
+	 /**
+     * Este método captura un digimon en el caso de que su salud sea
+     * menor o igual a 20 y lo añade al equipo
+     * 
+     * @param digimon El Digimon capturado
+     */
+	
 	public void captura(DigimonesIconicos digimon) {
 		if (digimon.Salud <= 20) {
 			Equipo.add(digimon);
@@ -30,6 +59,14 @@ public class Domador {
 		}
 	}
 
+	
+	 /**
+     * Este método comprueba si uno de los 3 digimones ha sido capturado 
+     * 
+     * @param nombre Nombre del digimon que va a ser comprobado
+     * @return Devuelve true si el digimon ha sido capturado o false sino lo ha sido
+     */
+	
 	public boolean DigimonCapturado(String nombre) {
 		for (DigimonesIconicos digimon : Equipo) {
 			if (digimon.Nombre.equals(nombre)) {
