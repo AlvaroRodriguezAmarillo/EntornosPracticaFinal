51e2dc383aae510741018b0e55d10befb9d52f58
Álvaro Rodríguez Amarillo <145559192+AlvaroRodriguezAmarillo@users.noreply.github.com>
2024-05-28 13:00:45 +0200

    Principal comentado definitivo

--- actividad/Principal.java
+++ actividad/Principal.java
@@ -1,58 +1,74 @@
 package actividad;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * La clase Principal es el comienzo del programa, desde el que el usuario tendrá que elegir las diferentes opciones
+ * La clase Principal es el comienzo del programa, desde el que el usuario tendrá que elegir las diferentes opciones.
  *
  * @version 1.0
- * @author Álvaro
+ * @autor Álvaro
  */
-
 public class Principal {
 
-	/**
-	 * Este método permite al usuario ingresar su nombre de domador y elegir si quiere
-	 * iniciar una batalla o salir.
-	 */
-	
-	public static void main(String[] args) {
+    /**
+     * Este método permite al usuario ingresar su nombre de domador y elegir si quiere
+     * iniciar una batalla o salir.
+     *
+     * @throws InputMismatchException En caso de que el usuario escriba un dato incorrecto
+     * @throws Exception Cualquier xcepción que se pueda dar
+     */
+    public static void main(String[] args) {
 
-		Scanner leer = new Scanner(System.in);
-		System.out.println("Bienvenido al mundo Digimon, por favor ingrese su nombre de Domador: ");
+        Scanner leer = new Scanner(System.in);
+        try {
+            System.out.println("Bienvenido al mundo Digimon, por favor ingrese su nombre de Domador: ");
+            String NombreDomador = leer.nextLine();
+            Domador domador = new Domador(NombreDomador);
+            
+            while (domador.Equipo.size() < 3) {
+                System.out.println();
+                System.out.println("--------------------------------------------------------------------------");
+                System.out.println();
+                System.out.println("Elige una de las opciones posibles:");
+                System.out.println();
+                System.out.println("1. Iniciar batalla");
+                System.out.println("2. Salir");
+                
+                int opcion = 0;
+                try {
+                    opcion = leer.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("Por favor, introduce un número válido.");
+                    leer.next(); 
+                    continue;
+                }
 
-		String NombreDomador = leer.nextLine();
-		Domador domador = new Domador(NombreDomador);
-		while (domador.Equipo.size() < 3) {
-			System.out.println();
-			System.out.println("--------------------------------------------------------------------------");
-			System.out.println();
-			System.out.println("Elige una de las opciones posibles:");
-			System.out.println();
-			System.out.println("1. Iniciar batalla");
-			System.out.println("2. Salir");
-			int opcion = leer.nextInt();
-			switch (opcion) {
+                switch (opcion) {
+                    case 1:
+                        BatallaDigital batalla = new BatallaDigital(domador);
+                        batalla.pelea();
+                        break;
 
-			case 1:
-				BatallaDigital batalla = new BatallaDigital(domador);
-				batalla.pelea();
-				break;
+                    case 2:
+                        break;
 
-			case 2:
-				break;
-
-			default:
-				System.out.println("Esta opción no es válida, elige una opción válida de entre las posibles.");
-				break;
-			}
-			if (opcion == 2) {
-				break;
-			}
-		}
-		if (domador.Equipo.size() == 3) {
-			System.out.println("Enhorabuena " + domador.Nombre + ". Has capturado los 3 Digimones.");
-		}
-		leer.close();
-	}
+                    default:
+                        System.out.println("Esta opción no es válida, elige una opción válida de entre las posibles.");
+                        break;
+                }
+                if (opcion == 2) {
+                    break;
+                }
+            }
+            
+            if (domador.Equipo.size() == 3) {
+                System.out.println("Enhorabuena " + domador.Nombre + ". Has capturado los 3 Digimones.");
+            }
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error: " + e.getMessage());
+        } finally {
+            leer.close();
+        }
+    }
 }
